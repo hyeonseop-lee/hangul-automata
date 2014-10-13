90eb9d945f2df99f59768b1dbdfbae281cb0000b
Lee Hyeonseop <protos37@gmail.com>
2014-10-13 14:03:47 +0900

    basic Mealy Machine implemented

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import DFA
+import ME
 
 A = DFA.DFA.FromFile("test.json")
-A.query("000", True)
-A.query("011", True)
-A.query("01101101", True)
-A.query("001101010011", True)
+print A.query("000", True)
+print A.query("011", True)
+print A.query("01101101", True)
+print A.query("001101010011", True)
+
+B = ME.ME.FromFile("test.json")
+print B.query("000", True)
+print B.query("011", True)
+print B.query("01101101", True)
+print B.query("001101010011", True)
